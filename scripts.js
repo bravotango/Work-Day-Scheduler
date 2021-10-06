ff233debe30516c876d473418123aebd8eebbd4a
seattleriot <seattleriot@hotmail.com>
2021-10-06 15:38:24 -0700

    moment.js time added to page, added monospace google font to display time

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
-$(".saveBtn").on("click", function (e) {
-  console.log(this);
-  var description = $(this).siblings(".description").val();
-  console.log(description);
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    const description = $(this).siblings(".description").val();
+    const parentId = $(this).parent().attr("id");
+    console.log(description, parentId);
+  });
+
+  let currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+  let timer;
+  function timeUpdater() {
+    timer = setInterval(function () {
+      currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+      $("#currentDay").text(currentDate);
+    }, 1000);
+  }
+  timeUpdater();
+  console.log("currentDate", currentDate);
 });

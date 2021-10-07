6cc9178bfef275b6db3df18edc84194063f169b2
seattleriot <seattleriot@hotmail.com>
2021-10-07 15:10:31 -0700

    removed placeholer time block from HTML, add applyClasses function

--- scripts.js
+++ scripts.js
@@ -2,56 +2,78 @@ $(document).ready(function () {
   let currentDate;
   let timer;
   let currentHour;
-  const workHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]; // military time to help with AM PM
-
-  function createFormTimeBlock(hour) {
-    const amPm = hour < 12 ? "AM" : "PM";
-    const formBlock = $("#formBlocks");
-    const form = $("<form>")
-      .attr("id", `time${hour}`)
-      .addClass(
-        "row time-block bg-secondary text-white rounded border-right border-bottom"
-      );
-
-    // hour container elements
-    // TODO: break into function
-    const timeContainer = $("<div>").addClass("col-sm-2 hour");
-    const pTime = $("<p>").addClass("float-left").text(`${hour}${amPm}`);
-    const buttonContainer = $("<div>").addClass(
-      "d-block d-sm-none float-right saveBtn"
-    );
-
-    const saveButtonMobile = createSaveButton();
-    const saveButton = createSaveButton();
-
-    buttonContainer.append(saveButtonMobile);
-    timeContainer.append(pTime);
-    timeContainer.append(buttonContainer);
-    form.append(timeContainer);
-
-    // textarea container
-    // TODO: break into function
-    const textarea = $("<textarea>").addClass("col-sm-8 col-lg-9 description");
-    textarea.prop("required", true);
-    form.append(textarea);
-
-    // save container
-    // TODO: break into function
-    const saveContainer = $("<div>").addClass(
-      "col-sm-2 col-lg-1 saveBtn d-none d-sm-block"
-    );
-    saveContainer.append(saveButton);
-    form.append(saveContainer);
-
-    formBlock.append(form);
-  }
+  const workHours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]; // military time
 
   for (let i = 0; i < workHours.length; i++) {
     createFormTimeBlock(workHours[i]);
   }
 
-  // TODO: why this worked and instead of using the same button in more than one place
-  // factory function
+  // event listener for a form .saveBtn clicked
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    const description = $(this).children(".description").val(); // value typed into the textarea associated (sibling) of saveBtn
+    const key = $(this).attr("id"); // key to use for local storage
+    // save to local storage
+    localStorage.setItem(key, description);
+  });
+
+  timeUpdater();
+  getLocalStorageData();
+
+  function createFormTimeBlock(hour) {
+    const formBlock = $("#formBlocks");
+    const form = $("<form>")
+      .attr("id", `time${hour}`)
+      .addClass("row time-block text-white rounded border-right border-bottom");
+
+    // hour container elements
+    const timeContainer = createTimeContainer(hour);
+    form.append(timeContainer);
+
+    // textarea container
+    const textarea = createTextareaContainer();
+    form.append(textarea);
+
+    // save container
+    const saveContainer = createSaveContainer();
+    form.append(saveContainer);
+
+    formBlock.append(form);
+  }
+
+  function createTimeContainer(hour) {
+    let amPm = hour < 12 || hour === 24 ? "AM" : "PM";
+    // convert from military time to standard time for display
+    const displayHour = hour <= 12 ? hour : hour - 12;
+    const timeContainer = $("<div>").addClass("col-sm-2 hour");
+    const pTime = $("<p>").addClass("float-left").text(`${displayHour}${amPm}`);
+    const buttonContainer = $("<div>").addClass(
+      "d-block d-sm-none float-right saveBtn"
+    );
+
+    const saveButtonMobile = createSaveButton();
+
+    buttonContainer.append(saveButtonMobile);
+    timeContainer.append(pTime);
+    timeContainer.append(buttonContainer);
+    return timeContainer;
+  }
+
+  function createTextareaContainer() {
+    const textarea = $("<textarea>").addClass("col-sm-8 col-lg-9 description");
+    textarea.prop("required", true);
+    return textarea;
+  }
+
+  function createSaveContainer() {
+    const saveContainer = $("<div>").addClass(
+      "col-sm-2 col-lg-1 saveBtn d-none d-sm-block"
+    );
+    const saveButton = createSaveButton();
+    saveContainer.append(saveButton);
+    return saveContainer;
+  }
+
   function createSaveButton() {
     const saveButton = $("<button>")
       .attr("type", "submit")
@@ -63,26 +85,13 @@ $(document).ready(function () {
     return saveButton;
   }
 
-  // event listener for a form .saveBtn clicked
-  $("form").on("submit", function (e) {
-    e.preventDefault();
-    const description = $(this).children(".description").val(); // value typed into the textarea associated (sibling) of saveBtn
-    const parentId = $(this).attr("id"); // parentId to use for local storage
-    console.log(parentId, description);
-    // save to local storage
-    localStorage.setItem(parentId, description);
-  });
-
   function timeUpdater() {
     timer = setInterval(function () {
       currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
       if (currentHour !== moment().hour()) {
         currentHour = moment().hour();
-        // call function to apply classes for past present future
-        // update classes: lookup removing classes: success, primary, default | then adding back the correct classes | loop over array
-
-        console.log(currentHour);
-        console.log("lets call a change function");
+        console.log("update classes");
+        applyClasses();
       }
       $("#currentDay").text(currentDate);
     }, 1000);
@@ -98,6 +107,16 @@ $(document).ready(function () {
     }
   }
 
-  timeUpdater();
-  getLocalStorageData();
+  function applyClasses() {
+    for (let i = 0; i < workHours.length; i++) {
+      const targetForm = $(`form#time${workHours[i]}`);
+      if (currentHour == workHours[i]) {
+        targetForm.addClass("bg-primary");
+      } else if (currentHour < workHours[i]) {
+        targetForm.addClass("bg-success");
+      } else if (currentHour > workHours[i]) {
+        targetForm.addClass("bg-secondary");
+      }
+    }
+  }
 });
